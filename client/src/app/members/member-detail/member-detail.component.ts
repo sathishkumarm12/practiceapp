0c8758aef674e133b93029dc67884be236d20de3
Sathishkumar Mohan <Sathishkumar.mohan@rheincs.com>
2021-10-13 14:35:55 +0530

    Section 9

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from '@kolkov/ngx-gallery';
+import { take } from 'rxjs/operators';
+import { Member } from 'src/app/models/member';
+import { MembersService } from 'src/app/services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberDetailComponent implements OnInit {
 
-  constructor() { }
+  member: Member;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
+  constructor(private memberService: MembersService, private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const username = this.activeRoute.snapshot.paramMap.get('username');
+    this.loadMember(username);
+
+    this.galleryOptions = [{
+      width: '500px',
+      height: '500px',
+      imagePercent: 100,
+      thumbnailsColumns: 4,
+      imageAnimation: NgxGalleryAnimation.Slide,
+      preview: false
+    }];
+
+
+  }
+
+  getImages(): NgxGalleryImage[] {
+    const imageUrls = [];
+
+    for (const image of this.member.photos) {
+      imageUrls.push({
+        small: image?.url,
+        medium: image?.url,
+        big: image?.url
+      });
+    }
+
+    return imageUrls;
+  }
+
+  loadMember(username: string) {
+    this.memberService.getMember(username).pipe(take(1)).subscribe(
+      memberData => { this.member = memberData; this.galleryImages = this.getImages(); }
+    );
   }
 
 }
---
+++ client/src/app/models/member.ts
@@ -0,0 +1,18 @@
+import { Photo } from './photo'
+
+export interface Member {
+    id: number;
+    userName: string;
+    age: number;
+    photoUrl: string;
+    knowAs: string;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos?: Photo[];
+}
+
